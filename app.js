f533aeba32bc5d7895061a371337eb206e885d7a
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-29 17:35:40 +0530

    setup

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const feedRoute = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+})
 
 app.use('/feed', feedRoute);
 
--- cotrollers/feed.js
+++ cotrollers/feed.js
@@ -6,3 +6,19 @@ exports.getPosts = (req, res, next) => {
         }]
     });
 };
+
+
+exports.createPost = (req, res, next) => {
+
+    const title = req.body.title;
+    const content = req.body.content;
+    
+    //create post in DB
+
+    res.status(201).json({
+        message: "Post created successfully!",
+        post:{
+            id: new Date().toISOString(), title: title, content: content
+        }
+    })
+}
