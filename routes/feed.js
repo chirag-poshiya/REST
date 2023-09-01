be537bb0822d32c193b45cf9a76980db8ccc776e
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-09-01 10:10:46 +0530

    rest

--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
-const feedController = require('../cotrollers/feed')
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-//GET /feed/posts
-router.get('/posts', feedController.getPosts);
+// GET /feed/posts
+router.get('/posts', isAuth, feedController.getPosts);
 
-// post /feed/post
-router.post('/post', feedController.createPost);
+// POST /feed/post
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
